7710d253227ac13c3d98ef0c4c61957601ed20cc
Narendar Gangula <gangulanarendar@gmail.com>
2018-02-23 15:39:05 +0530

    added server and client io

--- server/server.js
+++ server/server.js
@@ -1,16 +1,32 @@
 const express=require('express');
 const path=require('path');
 const http=require('http');
+const socketIO=require('socket.io');
 
 const publicPath=path.join(__dirname,'../public');
 console.log(publicPath);
 var port=process.env.PORT||3000;
 
 var app=new express();
+var server=http.createServer(app);
+var serverIo=socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+
+serverIo.on('connection',(socket)=>{
+
+    console.log('New User Connected..');
+
+    socket.on('disconnect',function(){
+        console.log('User discconnect');
+    });
+});
+
+
+server.listen(port,()=>{
 
     console.log('server is up at 3000'); 
-})
+
+
+});
 
