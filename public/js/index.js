eab2a1c359412df5cef10235ec8e9245937fdc0f
Narendar Gangula <gangulanarendar@gmail.com>
2018-02-26 18:28:03 +0530

    add loc and styles and mustache

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 var socket = io();
 
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -9,21 +10,28 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
+  var formattedTime=moment(message.createdAt).format('h:mm a');
   console.log('newMessage', message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  var template = $('#message-template').html();
+  var html=Mustache.render(template,{
+    from :message.from,
+    text:message.text,
+    createdAt:formattedTime
+  });
+  jQuery('#messages').append(html);
 
-  jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var formattedTime=moment(message.createdAt).format('h:mm a');
+  var template = $('#location-message-template').html();
+  var html=Mustache.render(template,{
+    from :message.from,
+    url:message.url,
+    createdAt:formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
